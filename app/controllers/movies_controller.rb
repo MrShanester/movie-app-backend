f760764f0148160cf56c05c733e6c99c79cf95b6
Shane Nicholson <shanescottnicholson@gmail.com>
2021-11-23 13:53:49 -0800

    Adds routes and controller CRUD actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,43 @@
 class MoviesController < ApplicationController
+  def index
+    output = Movie.all
+    render json: output
+  end
+
+  def show
+    input = params["id"]
+    output = Movie.find_by id: input
+    render json: output
+  end
+
+  def create
+    movie = Movie.new(title: params["title"], year: params["year"], plot: params["plot"], director: params["director"], english: params["english"])
+    if movie.save
+      render json: movie
+    else 
+      render json: {error: movie.errors.full_messages}, status: 406
+    end
+  end
+
+  def destroy
+    input = params["id"].to_i
+    movie = Movie.find_by id: input
+    movie.destroy
+    render json: {message: "Movie Annihilated"}
+  end
+
+  def update
+    input = params["id"]
+    movie = Movie.find_by id: input
+    movie.title = params["title"] || movie.title
+    movie.year = params["year"] || movie.year
+    movie.plot = params["plot"] || movie.plot
+    movie.director = params["director"] || movie.director
+    movie.english = params["english"] || movie.english
+    if movie.save
+      render json: movie
+    else 
+      render json: {error: movie.errors.full_messages}, status: 406
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/movies" => "movies#index"
+  post "/movies" => "movies#create"
+  get "/movies/:id" => "movies#show"
+  patch "/movies/:id" => "movies#update"
+  delete "/movies/:id" => "movies#destroy"
 end
